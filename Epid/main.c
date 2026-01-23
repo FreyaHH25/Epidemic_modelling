80ff91fab8b6bda5ece5d6b4a91b1ff19ea67ac3
sostro25 <sostro25@student.aau.dk>
2026-01-23 13:58:17 +0100

    noter
    noter tilføjet

--- Epid/main.c
+++ Epid/main.c
@@ -7,7 +7,7 @@
 // Mainfunktion
 int main(void)
 {
-    srand((unsigned int)time(NULL));
+    srand((unsigned int)time(NULL)); // starter ved et tilfældigt tal, da rand teknisk set ikke er tilfældig;
 
     brugerInput();
 
